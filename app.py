12a664b7937987c4c3423733101309167a3bfaab
iceballos <imceballos@uc.cl>
2023-03-30 22:20:01 -0300

    Updates

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from fastapi.responses import FileResponse
 from passlib.context import CryptContext
 
 from fastapi.responses import HTMLResponse, Response
-
+from pydantic import BaseModel
+import json
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import List, Annotated
 from jose import JWTError, jwt
-
+import base64
 from datetime import datetime, timedelta
 
 import os
@@ -82,7 +83,7 @@ def get_write_func_filename(option: str) -> str:
     return mapper.get(option, "")
     
 @app.get("/")
-async def index(request: Request, user: User = Depends(get_current_user_from_token)):
+async def index(request: Request):
     """
     gets the list of files from the directory, creates a list of dictionaries with the name and size fields, and returns an HTML response
     with the generated file list
@@ -91,12 +92,19 @@ async def index(request: Request, user: User = Depends(get_current_user_from_tok
     files = [{"name": file, "size": os.path.getsize(os.path.join("test_files", file))} for file in file_list]
     return templates.TemplateResponse("index.html", {"request": request, "files": files})
 
-@app.get("/download/{filename}")
-async def download_file(filename: str):
+@app.get("/download/{folder}/{filename}")
+async def download_file(folder: str, filename: str):
     """
     Endpoint that returns the content of a file as a download
     """
-    file_path = os.path.join("test_files", filename)
+    encoded_bytes = folder.encode('ascii')
+
+    # Use Base64 to decode the bytes
+    decoded_bytes = base64.b64decode(encoded_bytes)
+
+    # Convert the decoded bytes to a string
+    decoded_text = decoded_bytes.decode('ascii')
+    file_path = os.path.join(decoded_text, filename)
     return FileResponse(file_path, media_type="application/octet-stream", filename=filename)
 
 
@@ -139,9 +147,16 @@ async def process_form(file: UploadFile, request: Request, user: User = Depends(
     
     return templates.TemplateResponse("table.html", {"data": data, "request": request})
 
-@app.post("/delete_file/{filename}")
-async def delete_file(filename: str, request: Request):
-    file_path = os.path.join("test_files", filename)
+@app.post("/delete_file/{filename}/{folder}")
+async def delete_file(request: Request, filename: str, folder: str):
+    encoded_bytes = folder.encode('ascii')
+
+    # Use Base64 to decode the bytes
+    decoded_bytes = base64.b64decode(encoded_bytes)
+
+    # Convert the decoded bytes to a string
+    decoded_text = decoded_bytes.decode('ascii')
+    file_path = os.path.join(decoded_text, filename)
     try:
         os.remove(file_path)
         return {"message": f"Successfully deleted {filename}"}
@@ -283,9 +298,21 @@ async def http_exception_handler(request, exc):
     return exc
 
 
-@app.get("/view_xml/{filename}", response_class=HTMLResponse)
-async def view_xml(request: Request, filename: str):
-    file_path = os.path.join("test_files", filename)
+@app.get("/view_xml/{filename}/{folder}/{status}", response_class=HTMLResponse)
+async def view_xml(request: Request, filename: str, folder: str, status: str):
+    print("ESTOY ACA")
+    print(filename)
+    print(folder)
+    print(status)
+    encoded_bytes = folder.encode('ascii')
+
+    # Use Base64 to decode the bytes
+    decoded_bytes = base64.b64decode(encoded_bytes)
+
+    # Convert the decoded bytes to a string
+    decoded_text = decoded_bytes.decode('ascii')
+    file_path = f"{decoded_text}/{status}/{filename}"
+    print(filename, folder, file_path, decoded_text)
     with open(file_path, "r") as file:
         contents = file.read()
 
@@ -293,3 +320,118 @@ async def view_xml(request: Request, filename: str):
     data = await xml_parser(contents)
 
     return templates.TemplateResponse("table.html", {"data": data, "request": request, "filename": filename})
+
+@app.get("/create_connection", response_class=HTMLResponse)
+async def create_connection(request: Request):
+    folder_path = "test_files"
+    folder_names = [name for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
+    return templates.TemplateResponse("connections.html", {"request": request, "folders": folder_names})
+
+
+@app.get("/folder/{folder_name}")
+async def folder_detail(request: Request, folder_name: str):
+    folder_path = f"test_files/{folder_name}"
+    if not os.path.isdir(folder_path):
+        return responses.PlainTextResponse("Folder not found", status_code=404)
+    files = os.listdir(folder_path)
+    return templates.TemplateResponse("folder.html", {"request": request, "folder_name": folder_name, "files": files})
+
+@app.post("/create_connection")
+async def create_connection_post(request: Request,
+    name: str = Form(...),
+    location: str = Form(...),
+    company: str = Form(...)
+):
+    # Create the connection object
+    #connection = Connection(name=name, location=location, company=company)
+    folder_path = "test_files"
+
+    # Create the folder
+    folder_name = f"test_files/{name}_{company}"
+    required_subfolders = ["request_to_trucker", "acknowledge", 
+                "trucker_response", "trucker_event_instruction_planning",
+                "trucker_event_instruction_actual", "arrival_on_site","pod_ppu"]
+    os.mkdir(folder_name)
+    for subfolder in required_subfolders:
+        os.mkdir(f"{folder_name}/{subfolder}")
+        os.mkdir(f"{folder_name}/{subfolder}/accepted")
+        os.mkdir(f"{folder_name}/{subfolder}/rejected")
+        os.mkdir(f"{folder_name}/{subfolder}/pending")
+
+    # TODO: Save the connection and folder information to a database
+
+    # Return a success message
+    folders = [name for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
+    # Render the HTML template with the folder names
+    context = {"request": request, "folders": folders}
+    return templates.TemplateResponse("connections.html", context=context)
+
+class OperationRequest(BaseModel):
+    operation_id: dict
+
+@app.get("/get_template")
+async def perform_operation(request: Request, folder_path: str):
+    #folder_path = request.query_params.get("folder_path", "")
+    #folder_path = json.loads(folder_path) if folder_path else []
+    file_list = os.listdir(folder_path)
+    
+    text_bytes = folder_path.encode('ascii')
+
+    # Use Base64 to encode the bytes
+    encoded_bytes = base64.b64encode(text_bytes)
+
+    # Convert the encoded bytes to a string
+    encoded_text = encoded_bytes.decode('ascii')
+    accepted_files = [{"name": file, "size": os.path.getsize(os.path.join(f"{folder_path}/accepted", file)), "folder": encoded_text} for file in os.listdir(f"{folder_path}/accepted")]
+    rejected_files = [{"name": file, "size": os.path.getsize(os.path.join(f"{folder_path}/rejected", file)), "folder": encoded_text} for file in os.listdir(f"{folder_path}/rejected")]
+    pending_files = [{"name": file, "size": os.path.getsize(os.path.join(f"{folder_path}/pending", file)), "folder": encoded_text} for file in os.listdir(f"{folder_path}/pending")]
+
+    files = [{"name": file, "size": os.path.getsize(os.path.join(folder_path, file)), "folder": encoded_text} for file in file_list]
+    return templates.TemplateResponse("index.html", {"request": request, "accepted_files": accepted_files, "rejected_files": rejected_files, "pending_files": pending_files})
+
+
+@app.get("/showfolder")
+async def show_folder(request: Request):
+    return templates.TemplateResponse('folder.html', {"request": request})
+
+@app.post("/perform_operation1")
+async def perform_operation1(data: dict, request: Request):
+    operation_id = data.get("operation_id", "")
+    folder_name = data.get("folder_name", "")
+    operation_folders = {
+        1: "request_to_trucker",
+        2: "acknowledge",
+        3: "trucker_response",
+        4: "trucker_event_instruction_planning",
+        5: "trucker_event_instruction_actual",
+        6: "arrival_on_site",
+        7: "pod_ppu"
+    }
+    folder_path = f"test_files/{folder_name}/{operation_folders[operation_id]}"
+    xml_files = [f for f in os.listdir(folder_path) if f.endswith(".xml")]
+    
+    # Do some operation here based on the operation_id
+    # Render the corresponding template and return it as an HTML response
+    return {"url": "/get_template", "data": 1, "files":  xml_files, "folder_path": folder_path}
+
+class File(BaseModel):
+    name: str
+    folder: str
+    status: str
+    currentstatus: str
+
+@app.post("/update_file_status")
+def update_file_status(files: List[File]):
+    files = files[0]
+    encoded_bytes = files.folder.encode('ascii')
+    # Use Base64 to decode the bytes
+    decoded_bytes = base64.b64decode(encoded_bytes)
+    current_status = files.currentstatus
+    # Convert the decoded bytes to a string
+    decoded_text = decoded_bytes.decode('ascii')
+    # construct the full paths for the source and destination files
+    source_file = os.path.join(decoded_text, current_status , files.name)
+    destination_file =  os.path.join(decoded_text, files.status , files.name)
+    # use os.rename() to move the file from the source to the destination folder
+    os.rename(source_file, destination_file)
+    return {"message": "Successfully updated"}
