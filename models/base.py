647382e09763bb37d55c903598ebc5e85ac2f5c1
iceballos <imceballos@uc.cl>
2023-04-06 10:33:50 -0400

    Tentative solution base

--- models/base.py
+++ models/base.py
@@ -7,34 +7,51 @@ Base = declarative_base()
 class Person(Base):
     __tablename__ = "people"
 
-    ssn = Column("ssn", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True)
     first_name = Column("first_name", String)
     last_name = Column("last_name", String)
     email = Column("email", String)
 
-    def __init__(self, ssn, first_name, last_name, email):
-        self.ssn = ssn
+    def __init__(self, id, first_name, last_name, email):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
 
     def __repr__(self):
-        return f"{self.ssn} {self.first_name} {self.email}"
+       return f"{self.id} {self.first_name} {self.last_name} {self.email}"
 
     @classmethod
     def find_by_email(cls, email):
-        return cls.query.filter_by(email=email).first()
+        session = cls._session()
+        return session.query(cls).filter_by(email=email).first()
 
     @classmethod
     def find_by_public_id(cls, public_id):
-        return cls.query.filter_by(public_id=public_id).first()
+        session = cls._session()
+        return session.query(cls).filter_by(public_id=public_id).first()
 
-engine = create_engine("sqlite:///mydb.db", echo=True)
-Base.metadata.create_all(bind=engine)
+    @classmethod
+    #Definir classmethods para: Buscar por first_name, last_name, por id, por email
+    #Agregar atributo company al tablero users, agregar un classmethod para company
+    #Agregar verificadores de email, de caracteres sanitizados y setear un largo maximo de VARCHAR(60-80) 
+    #Files: uuid, filename, path, condition
+    #Crear metodos de clase para filename, path, contenido dentro de, condition.
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    @classmethod
+    def _session(cls):
+        engine = create_engine("sqlite:///mydb.db")
+        Session = sessionmaker(bind=engine)
+        return Session()
 
-person = Person(1234, "mIKE", "Smith", "imceballso@gmail.com")
-session.add(person)
-session.commit()
+#engine = create_engine("sqlite:///mydb.db", echo=True)
+#Base.metadata.create_all(bind=engine)
+
+#Session = sessionmaker(bind=engine)
+#session = Session()
+
+#person = Person(1, "Israel", "Ceballos", "imceballos@gmail.com")
+#session.add(person)
+#session.commit()
+
+print(Person.find_by_email("imceballos@gmail.com"))
