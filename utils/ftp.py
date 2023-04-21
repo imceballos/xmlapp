d35ece92be07cac266d0c818a8ad20b454212d09
iceballos <imceballos@uc.cl>
2023-04-21 18:42:14 -0400

    Add FTP

--- utils/ftp.py
+++ utils/ftp.py
@@ -1,20 +1,30 @@
+from ftplib import FTP
+import os
 
 class FTPDownloader:
     def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password =  password
+        self.ftp = None
  
- ftp = FTP(ip)
-    ftp.login(user=username, passwd=password)
-    
-    if action == "Upload":
-        ftp.storbinary(f"STOR {file.filename}", file.file)
-    
-    elif action == "Download":
-        files = ftp.nlst()
-        for file in files:
-            if len(file)>3 and "." in file:
-                with open(f"folder/{file}", "wb") as f:
-                    ftp.retrbinary(f"RETR {file}", f.write)
+    def connect(self):
+        """Establish an connection and return an FTP client object"""
+
+        if self.ftp is None:
+            self.ftp = FTP(self.ip)
+            self.ftp.login(user=self.username, passwd=self.password)
+
+        return self.ftp
         
-    ftp.quit()
-    
-    return {"message": f"File {action}ed successfully"}
+    def download_files(self, local_path):
+        ftp = self.connect()
+        files = ftp.nlst()
+        local_elements = os.listdir(local_path)
+        for file in files:
+            if self.check_file(file, local_elements):
+                with open(f"{local_path}/{file}", "wb") as f:
+                   ftp.retrbinary(f"RETR {file}", f.write)
+            
+    def check_file(self, file, local_elements):
+        return len(file) > 3 and "." in file and file[0] != "." and file not in local_elements
