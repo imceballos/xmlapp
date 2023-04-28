ee237a533350062c06cae55de3434fc4e136ca6e
iceballos <imceballos@uc.cl>
2023-04-27 22:36:17 -0400

    Last Update

--- models/create_user.py
+++ models/create_user.py
@@ -16,6 +16,8 @@ p3.save()
 p2 = Person("Gonzalo", "Uribe", "gonzalo@gmail.com", "password2", "", "spvrigo")
 p2.save()
 
+p1 = Person("Israel", "Ceballos", "israel@gmail.com", "password1", "", "spvrigo")
+p1.save()
 
 p3 = Person.find_by_email("jesus@gmail.com")
 print(p3.first_name)
